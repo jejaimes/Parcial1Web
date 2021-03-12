c31cd3f1c36499e551babad3bdcc42febccb27a6
Julian Jaimes <je.jaimes@uniandes.edu.co>
2021-03-12 15:51:56 -0500

    funcionalidades 1 y 22 hechas

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
 const url = './productos.json'
+let listaProductos
+const categorias = ['burgers', 'tacos', 'salads', 'desserts', 'drinks&sides']
+
+function addItem () {
+  const items = document.getElementById('items')
+  let cont = items.textContent.split(' ')
+  cont = parseInt(cont[0])
+  cont++
+  items.textContent = cont + ' items'
+}
 
 function crearCardsProductos (productos) {
-  const contenido = document.getElementsById('productos')
+  const contenido = document.getElementById('productos')
+  contenido.textContent = ''
+  let col
   let card
   let img
   let cardBody
@@ -10,44 +22,61 @@ function crearCardsProductos (productos) {
   let cardPrice
   let cardButton
   productos.forEach(element => {
+    col = document.createElement('div')
+    col.className = 'col'
     card = document.createElement('div')
-    card.className('card')
-    card.style('width: 18rem;')
+    card.className = 'card h-100'
     img = document.createElement('img')
-    img.src(element.image)
-    img.className('card-img-top')
-    img.alt('Imagen del producto')
+    img.src = element.image
+    img.className = 'card-img-top'
+    img.alt = 'Imagen del producto'
     cardBody = document.createElement('div')
-    cardBody.className('card-body')
+    cardBody.className = 'card-body'
     cardTitle = document.createElement('h5')
-    cardTitle.className('card-title')
-    cardTitle.textContent(element.name)
+    cardTitle.className = 'card-title bold'
+    cardTitle.textContent = element.name
     cardText = document.createElement('p')
-    cardText.className('card-text')
-    cardText.textContent(element.description)
+    cardText.className = 'card-text'
+    cardText.textContent = element.description
     cardPrice = document.createElement('p')
-    cardPrice.className('card-text price')
-    cardPrice.textContent(element.price)
+    cardPrice.className = 'card-text bold'
+    cardPrice.textContent = element.price
     cardButton = document.createElement('a')
-    cardButton.className('btn btn-dark')
-    cardButton.textContent('Add to car')
+    cardButton.className = 'btn btn-dark'
+    cardButton.textContent = 'Add to car'
+    cardButton.onclick = addItem
     cardBody.appendChild(cardTitle)
     cardBody.appendChild(cardText)
     cardBody.appendChild(cardPrice)
     cardBody.appendChild(cardButton)
     card.appendChild(img)
     card.appendChild(cardBody)
-    contenido.appendChild(card)
+    col.appendChild(card)
+    contenido.appendChild(col)
   })
 }
 
-/*
-<div class="card" style="width: 18rem;">
-          <img src="" class="card-img-top" alt="...">
-          <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text"></p>
-            <p class="card-text"></p>
-            <a href="#" class="btn btn-dark">Go somewhere</a>
-          </div>
-        </div>*/
+const promise = new Promise(function (resolve, reject) {
+  fetch(url).then(function (response) {
+    response.text().then((text) => {
+      resolve(text)
+    })
+  }).catch((err) => {
+    reject(err)
+  })
+})
+
+promise.then((text) => {
+  listaProductos = JSON.parse(text)
+  crearCardsProductos(listaProductos[0].products)
+  categorias.forEach(element => {
+    document.getElementById(element).onclick = displayCategoria
+  })
+})
+
+function displayCategoria () {
+  const index = categorias.findIndex((element) => element == this.id)
+  const categoria = document.getElementById('categoria')
+  categoria.textContent = listaProductos[index].name
+  crearCardsProductos(listaProductos[index].products)
+}
