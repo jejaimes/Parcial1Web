194701ba353fb458171b7721fc1061e91b9e07f8
Julian Jaimes <je.jaimes@uniandes.edu.co>
2021-03-12 20:57:31 -0500

    funcionalidades 3 y 4 listas

--- script.js
+++ script.js
@@ -1,6 +1,44 @@
 const url = './productos.json'
 let listaProductos
 const categorias = ['burgers', 'tacos', 'salads', 'desserts', 'drinks&sides']
+let order = []
+
+function addQuantity () {
+  const amounts = document.getElementsByClassName('amount')
+  const prices = document.getElementsByClassName('price')
+  const quantities = document.getElementsByClassName('quantity')
+  const total = document.getElementById('total')
+  order[this.value].quantity++
+  amounts[this.value].textContent = parseFloat(amounts[this.value].textContent) + parseFloat(prices[this.value].textContent)
+  quantities[this.value].textContent = parseFloat(quantities[this.value].textContent) + 1
+  total.textContent = 'Total: $' + (parseFloat(total.textContent.split('$')[1]) + parseFloat(prices[this.value].textContent))
+}
+
+function reduceQuantity () {
+  const amounts = document.getElementsByClassName('amount')
+  const prices = document.getElementsByClassName('price')
+  const quantities = document.getElementsByClassName('quantity')
+  const total = document.getElementById('total')
+  if (parseInt(quantities[this.value].textContent) === 1) {
+    order.splice(this.value, 1)
+    orderDetail()
+  } else {
+    order[this.value].quantity--
+    amounts[this.value].textContent = parseFloat(amounts[this.value].textContent) - parseFloat(prices[this.value].textContent)
+    quantities[this.value].textContent = parseFloat(quantities[this.value].textContent) - 1
+    total.textContent = 'Total: $' + (parseFloat(total.textContent.split('$')[1]) - parseFloat(prices[this.value].textContent))
+  }
+}
+
+function cancelOrder () {
+  
+}
+
+function confirmOrder () {
+  console.log(order)
+  //order = []
+  //orderDetail()
+}
 
 function addItem () {
   const items = document.getElementById('items')
@@ -8,11 +46,104 @@ function addItem () {
   cont = parseInt(cont[0])
   cont++
   items.textContent = cont + ' items'
+  const detail = this.value.split(';')
+  const index = order.findIndex(element => element.description === detail[0])
+  if (index !== -1) {
+    order[index].quantity++
+  } else {
+    const p = {
+      item: order.length + 1,
+      quantity: 1,
+      description: detail[0],
+      unitPrice: detail[1]
+    }
+    order.push(p)
+  }
+}
+
+function displayCategoria () {
+  const index = categorias.findIndex((element) => element === this.id)
+  const categoria = document.getElementById('categoria')
+  categoria.textContent = listaProductos[index].name
+  crearCardsProductos(listaProductos[index].products)
+}
+
+function orderDetail () {
+  const categoria = document.getElementById('categoria')
+  categoria.textContent = 'Order detail'
+  const contenido = document.getElementById('productos')
+  contenido.innerHTML = ''
+  const tabla = document.createElement('table')
+  tabla.className = 'table table-striped'
+  const tablaH = document.createElement('thead')
+  tablaH.innerHTML = '<tr><th>Item</th><th>Qty.</th><th>Description</th><th>Unit Price</th><th>Amount</th><th>Modify</th></tr>'
+  const tablaB = document.createElement('tbody')
+  let tr
+  let td
+  let btnGroup
+  let btn1
+  let btn2
+  let total = 0
+  for (let i = 0; i < order.length; i++) {
+    const element = order[i]
+    const amount = element.unitPrice * element.quantity
+    tr = document.createElement('tr')
+    td = document.createElement('td')
+    tr.innerHTML = '<td>' + element.item + '</td><td class="quantity">' + element.quantity + '</td><td>' + element.description + '</td><td class="price">' + element.unitPrice + '</td><td class="amount">' + amount + '</td>'
+    total += amount
+    btnGroup = document.createElement('div')
+    btnGroup.className = 'row-cols-2 d-inline-flex'
+    btn1 = document.createElement('button')
+    btn1.className = 'btn btn-secondary m-1'
+    btn1.textContent = '+'
+    btn1.onclick = addQuantity
+    btn1.value = i
+    btn2 = document.createElement('button')
+    btn2.className = 'btn btn-secondary m-1'
+    btn2.textContent = '-'
+    btn2.onclick = reduceQuantity
+    btn2.value = i
+    btnGroup.appendChild(btn1)
+    btnGroup.appendChild(btn2)
+    td.appendChild(btnGroup)
+    tr.appendChild(td)
+    tablaB.appendChild(tr)
+  }
+  const row = document.createElement('div')
+  row.className = 'row w-100'
+  const col1 = document.createElement('div')
+  col1.className = 'col-10'
+  const col2 = document.createElement('div')
+  col2.className = 'col-2'
+  const p = document.createElement('p')
+  p.className = 'bold'
+  p.id = 'total'
+  p.textContent = 'Total: $' + total
+  col1.appendChild(p)
+  btnGroup = document.createElement('div')
+  btnGroup.className = 'row-cols-2 d-inline-flex'
+  btn1 = document.createElement('button')
+  btn1.className = 'btn btn-outline-dark cancel m-1 p-1'
+  btn1.textContent = 'Cancel'
+  btn1.onclick = cancelOrder
+  btn2 = document.createElement('button')
+  btn2.className = 'btn btn-outline-dark confirm m-1 p-1 w-75'
+  btn2.textContent = 'Confirm order'
+  btn2.onclick = confirmOrder
+  btnGroup.appendChild(btn1)
+  btnGroup.appendChild(btn2)
+  col2.appendChild(btnGroup)
+  row.appendChild(col1)
+  row.appendChild(col2)
+  tabla.appendChild(tablaH)
+  tabla.appendChild(tablaB)
+  contenido.appendChild(tabla)
+  contenido.appendChild(row)
 }
 
 function crearCardsProductos (productos) {
   const contenido = document.getElementById('productos')
-  contenido.textContent = ''
+  contenido.innerHTML = ''
   let col
   let card
   let img
@@ -41,10 +172,11 @@ function crearCardsProductos (productos) {
     cardPrice = document.createElement('p')
     cardPrice.className = 'card-text bold'
     cardPrice.textContent = element.price
-    cardButton = document.createElement('a')
+    cardButton = document.createElement('button')
     cardButton.className = 'btn btn-dark'
     cardButton.textContent = 'Add to car'
     cardButton.onclick = addItem
+    cardButton.value = element.name + ';' + element.price
     cardBody.appendChild(cardTitle)
     cardBody.appendChild(cardText)
     cardBody.appendChild(cardPrice)
@@ -72,11 +204,5 @@ promise.then((text) => {
   categorias.forEach(element => {
     document.getElementById(element).onclick = displayCategoria
   })
+  document.getElementById('carrito').onclick = orderDetail
 })
-
-function displayCategoria () {
-  const index = categorias.findIndex((element) => element == this.id)
-  const categoria = document.getElementById('categoria')
-  categoria.textContent = listaProductos[index].name
-  crearCardsProductos(listaProductos[index].products)
-}
